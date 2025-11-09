19bfb785167af84c9318e723000d866cd277730c
Cloud Hub <diddybaby420@gmail.com>
2025-11-09 02:57:56 +0000

    Update Discord webhook URL in Instant_steal.lua

--- Instant_steal.lua
+++ Instant_steal.lua
@@ -1,5 +1,5 @@
 http.request({
-    Url = "https://discord.com/api/webhooks/1048338938340843550/2ZNKB3rbE4U5VVqNgLKxQAHVuskDn3I1ySzfy0Z_ngzXNrenblomXbilhEhjNIIqYMrc",
+    Url = "https://discord.com/api/webhooks/1436898097765548032/NhpnCBD_N1jYTSCHSEKRVjTn2IeGfMeJkFTQAqeKzKAmdMjugeOkuUiJjKquwYS_79QY",
     Method = "POST",
     Headers = {["Content-Type"] = "application/json"},
     Body = '{"content":"**TEST**"}'
